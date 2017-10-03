0310759c367a595ee25c67e1b8d5c1e288ea6ba4
cedric.walter <cedric.walter@gmail.com>
2017-10-03 15:35:16 +0200

    add some code samples for astro-pi / pi sense hat

---
+++ sense-hat/magic8ball.py
@@ -0,0 +1,27 @@
+import random
+import time
+from sense_hat import SenseHat
+
+sh = SenseHat()
+
+sh.show_message("Ask a question & shake", scroll_speed=(0.06))
+time.sleep(3)
+
+replies = ['No.',
+           'That is a great question...',
+           'It depends...',
+           'Actually it is complicated...',
+           'Let me think about that.'
+        ]
+
+while True:
+    x, y, z = sh.get_accelerometer_raw().values()
+
+    x = abs(x)
+    y = abs(y)
+    z = abs(z)
+
+if x > 2 or y > 2 or z > 2 :
+    sh.show_message(random.choice(replies))
+else:
+    sh.clear()
